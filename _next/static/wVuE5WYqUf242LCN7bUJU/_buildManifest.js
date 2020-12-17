a1098834f7fceeddc1be76e13496ac4f5dc98b75
nahuelwexd <nahual_gomca@outlook.com.ar>
2020-12-17 17:16:02 +0000

    Deploying to gh-pages from  @ 49088c0777749ddd7c19705e475cc38933806f89 🚀

---
+++ _next/static/wVuE5WYqUf242LCN7bUJU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-aae157f2af6ac12d37b5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-32995e81a2e5eebb9ce2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
