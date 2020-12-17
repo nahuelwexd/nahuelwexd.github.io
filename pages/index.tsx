a237a411acdd223d4215adb03b03808341e5e1ee
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-12-17 02:08:13 -0300

    Improved code style

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,23 @@ import Head from 'next/head'
 export default function Home() {
 	return (
 		<>
+
 			<Head>
-				<title>Nahuel Gomez Castro</title>
+
+				<title>
+					Nahuel Gomez Castro
+				</title>
+
 			</Head>
-			<h1>Nahuel Gomez Castro</h1>
-			<p>I'm a software developer based in Argentina</p>
+
+			<h1>
+				Nahuel Gomez Castro
+			</h1>
+
+			<p>
+				I'm a software developer based in Argentina
+			</p>
+
 		</>
 	)
 }
