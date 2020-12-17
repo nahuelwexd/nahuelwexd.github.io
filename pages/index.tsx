de2c422a7b8bf6210b73676d133a7bef667e6c57
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-12-16 21:00:26 -0300

    Added index page

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Nahuel Gomez Castro</title>
+			</Head>
+			<h1>Nahuel Gomez Castro</h1>
+			<p>I'm a software developer based in Argentina</p>
+		</>
+	)
+}
