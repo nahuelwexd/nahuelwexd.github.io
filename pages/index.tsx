e7a40ab00b28417070d1374612bdddf319c2ff72
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-12-17 02:15:52 -0300

    Contents wrapped in a container

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
 	return (
-		<>
+		<div className="container mx-auto">
 
 			<Head>
 
@@ -20,6 +20,6 @@ export default function Home() {
 				I'm a software developer based in Argentina
 			</p>
 
-		</>
+		</div>
 	)
 }
